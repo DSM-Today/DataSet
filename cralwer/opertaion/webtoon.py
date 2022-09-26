e4b05ea8c0c44c3386075dcb907112046e864ece
osangu <osangu012@gmail.com>
2022-09-26 14:33:52 +0900

    ⚡️ :: 웹툰 크롤링

--- cralwer/opertaion/webtoon.py
+++ cralwer/opertaion/webtoon.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup as bs
 from datetime import datetime, timedelta
 from random import randrange
 
-import pprint
+from pprint import pprint
 
 
 def get_weeday():
@@ -14,41 +14,47 @@ def get_weeday():
 class WebToon:
 
     def __init__(self):
-        self._basic_uri = 'https://comic.naver.com/webtoon'
-        self._list_uri = self._basic_uri + '/weekday'
+        self._basic_uri = 'https://comic.naver.com'
+        self._list_uri = self._basic_uri + '/webtoon/weekday'
 
     def get_today_toon_list(self):
         content = bs(get(self._list_uri).text, 'lxml')
 
-        today_content = content.findAll(class_='col_inner')[0]# [get_weeday()]
-        today = today_content.find_all_next('a')
+        today_content = content.findAll(class_='col_inner')[get_weeday()].ul
 
-        return self.parse_today_toon_list(today)
+        return self.parse_today_toon_list([today_content.li] + today_content.li.find_next_siblings())
 
+    @staticmethod
+    def get_toon_detail(web_toon_list: list):
+        rank = randrange(0, len(web_toon_list))
+        chosen_toon = web_toon_list[rank]
+        content = bs(get(chosen_toon['direct_url']).text, 'lxml').find(class_='detail')
 
-    def parse_today_toon_list(self, html_content):
+        return {
+            'status': chosen_toon['status'],
+            'rank': rank,
+            'published': chosen_toon['published'],
+            'direct_url': chosen_toon['direct_url'],
+            'title': chosen_toon['title'],
+            'image_path': chosen_toon['image_path'],
+            'writer': content.find(class_='wrt_nm').text.strip(),
+            'introduction': content.find('p').text.strip(),
+            'genre': content.find(class_='genre').text.strip()
+        }
+
+    def parse_today_toon_list(self, html_content_list):
         toon_list = []
 
-        for ind, val in enumerate(html_content):
-            if ind % 2 == 1:
-                continue
-            try:
-                str_val = str(val)
-                toon_info = {
-                    'status': val.text.strip(),  # 휴재 & 18세 이상 이용 가능 & NEW
-                    'direct_url': self._basic_uri + str_val[str_val.index('<a href="/webtoon') + 17: str_val.index(
-                        '" onclick')],
-                    'title': str_val[str_val.index('<img alt="') + 10: str_val.index('" height=')],
-                    'image_path': str_val[str_val.index(" src=") + 6: str_val.index('" title=')]
+        for val in html_content_list:
+            toon_list.append(
+                {
+                    'status': val.find('span',{'class':'mark_adult_thumb'}).text if val.find('span',{'class':'mark_adult_thumb'}) is not None else None,
+                    'published': '휴재' if val.find('em').get_text() == '휴재' else '연의',
+                    'title': val.img['alt'],
+                    'direct_url': self._basic_uri + val.a.attrs['href'],
+                    'image_path': val.img['src']
                 }
+            )
 
-                toon_list.append(toon_info)
-            except:
-                pass
         return toon_list
-
-
-if __name__ == '__main__':
-    toon = WebToon()
-    toon_list = toon.get_today_toon_list()
 
