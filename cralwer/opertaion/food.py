0271584c922dce974d92e73fe02133013ea40926
rlarldud1234 <rlarldud6482@naver.com>
2022-10-11 11:14:41 +0900

    ⚡️ :: 카멜케이스를 스네이크케이스로 바꿨어요

--- cralwer/opertaion/food.py
+++ cralwer/opertaion/food.py
@@ -4,25 +4,25 @@ from time import sleep
 import requests
 from bs4 import BeautifulSoup as bs
 
-class Food:
-    def fetchFood(self):
-        driver = webdriver.Chrome("../chromedriver")
-        driver.get("http://dogumaster.com/select/menu/")
 
-        driver.find_element(By.XPATH, '//*[@id="section_search"]').find_element(By.ID, "input_submit").click()
+def fetch_food():
+    driver = webdriver.Chrome("../chromedriver")
+    driver.get("http://dogumaster.com/select/menu/")
 
-        sleep(3)
+    driver.find_element(By.XPATH, '//*[@id="section_search"]').find_element(By.ID, "input_submit").click()
 
-        food_name = driver.find_element(By.XPATH, '//*[@id="section_search"]/form/div[4]/p').text
+    sleep(3)
 
-
-        url = "https://www.google.co.kr/search?q={food_name}&sxsrf=ALiCzsZWgcEwVPp9s9R7qr0PdCdsKlPZqg:1665327432124&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjLrJqQtNP6AhVMtlYBHdSLBt8Q_AUoAXoECAEQAw&biw=1034&bih=839&dpr=2".format(food_name=food_name)
-        html = requests.get(url)
-        soup = bs(html.text, "html.parser")
-        food_image = soup.find_all('img')[1]['src']
+    food_name = driver.find_element(By.XPATH, '//*[@id="section_search"]/form/div[4]/p').text
 
 
-        return {
-            "food_name": food_name,
-            "food_image": food_image
-        }
+    url = "https://www.google.co.kr/search?q={food_name}&sxsrf=ALiCzsZWgcEwVPp9s9R7qr0PdCdsKlPZqg:1665327432124&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjLrJqQtNP6AhVMtlYBHdSLBt8Q_AUoAXoECAEQAw&biw=1034&bih=839&dpr=2".format(food_name=food_name)
+    html = requests.get(url)
+    soup = bs(html.text, "html.parser")
+    food_image = soup.find_all('img')[1]['src']
+
+
+    return {
+        "food_name": food_name,
+        "food_image": food_image
+    }
